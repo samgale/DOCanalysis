1f740c8b318a52c429378a446990fe64416dba97
Sam Gale <samueldgale@gmail.com>
2023-09-29 18:21:48 -0700

    sdg

---
+++ vbnAnalysisUtils.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 29 14:39:37 2023
+
+@author: svc_ccg
+"""
+
+import numpy as np
+import scipy.stats
+import sklearn
+
+
+def getFlashTimes(stim):
+    # stim = stimulus table or index of
+    flashTimes = np.array(stim['start_time'])
+    changeTimes = flashTimes[stim['is_change']]
+    hit = np.array(stim['hit'][stim['is_change']])
+    engaged = np.array([np.sum(hit[(changeTimes>t-60) & (changeTimes<t+60)]) > 1 for t in flashTimes])
+    autoRewarded = np.array(stim['auto_rewarded']).astype(bool)
+    changeFlashes = np.array(stim['is_change'] & ~autoRewarded & engaged)
+    nonChangeFlashes = np.array(engaged &
+                                (~stim['is_change']) & 
+                                (~stim['omitted']) & 
+                                (~stim['previous_omitted']) & 
+                                (stim['flashes_since_change']>5) &
+                                (stim['flashes_since_last_lick']>1))
+    lick = np.array(stim['lick_for_flash'])
+    lickTimes = np.array(stim['lick_time'])
+    lickLatency = lickTimes - flashTimes
+    earlyLick = lickLatency < 0.15
+    lateLick = lickLatency > 0.75
+    nonChangeFlashes[earlyLick | lateLick] = False
+    return flashTimes, changeFlashes, nonChangeFlashes, lick
+
+
+def findResponsiveUnits(basePsth,respPsth,baseWin,respWin):
+    hasSpikes = ((respPsth[:,:,respWin].mean(axis=(1,2)) - basePsth[:,:,baseWin].mean(axis=(1,2))) / 0.001) > 0.1
+    
+    base = basePsth[:,:,baseWin].mean(axis=1)
+    resp = respPsth[:,:,respWin].mean(axis=1)
+    peak = np.max(resp-base.mean(axis=1)[:,None],axis=1)
+    hasPeakResp = peak > 5 * base.std(axis=1)
+    
+    base = basePsth[:,:,baseWin].mean(axis=2)
+    resp = respPsth[:,:,respWin].mean(axis=2)
+    pval = np.array([1 if np.sum(r-b)==0 else scipy.stats.wilcoxon(b,r)[1] for b,r in zip(base,resp)])
+    
+    return hasSpikes & hasPeakResp & (pval<0.05)
+
+
+def crossValidate(model,X,y,nSplits):
+    # cross validation using stratified shuffle split
+    # each split preserves the percentage of samples of each class
+    # all samples used in one test set
+    
+    classVals = np.unique(y)
+    nClasses = len(classVals)
+    nSamples = len(y)
+    samplesPerClass = [np.sum(y==val) for val in classVals]
+    samplesPerSplit = [round(n/nSplits) for n in samplesPerClass]
+    shuffleInd = np.random.permutation(nSamples)
+    
+    cv = {'estimator': [sklearn.base.clone(model) for _ in range(nSplits)]}
+    cv['train_score'] = []
+    cv['test_score'] = []
+    cv['predict'] = np.full(nSamples,np.nan)
+    cv['predict_proba'] = np.full((nSamples,nClasses),np.nan)
+    cv['decision_function'] = np.full((nSamples,nClasses),np.nan) if nClasses>2 else np.full(nSamples,np.nan)
+    cv['feature_importance'] = []
+    cv['coef'] = []
+    modelMethods = dir(model)
+    
+    for k,estimator in enumerate(cv['estimator']):
+        testInd = []
+        for val,n in zip(classVals,samplesPerSplit):
+            start = k*n
+            ind = shuffleInd[y[shuffleInd]==val]
+            testInd.extend(ind[start:start+n] if k+1<nSplits else ind[start:])
+        trainInd = np.setdiff1d(shuffleInd,testInd)
+        estimator.fit(X[trainInd],y[trainInd])
+        cv['train_score'].append(estimator.score(X[trainInd],y[trainInd]))
+        cv['test_score'].append(estimator.score(X[testInd],y[testInd]))
+        cv['predict'][testInd] = estimator.predict(X[testInd])
+        for method in ('predict_proba','decision_function'):
+            if method in modelMethods:
+                cv[method][testInd] = getattr(estimator,method)(X[testInd])
+        for attr in ('feature_importance_','coef_'):
+            if attr in estimator.__dict__:
+                cv[attr[:-1]].append(getattr(estimator,attr))
+    return cv
+
--- vbnFigs.py
+++ vbnFigs.py
@@ -5,25 +5,100 @@ Created on Thu Sep 28 10:06:06 2023
 @author: svc_ccg
 """
 
-import glob, os, time, warnings
+import glob
+import os
 import numpy as np
 import pandas as pd
+import h5py
 import matplotlib
 import matplotlib.pyplot as plt
 matplotlib.rcParams['pdf.fonttype'] = 42
-import h5py
+from vbnAnalysisUtils import getFlashTimes
 
 
-baseDir = r'\\allen\programs\mindscope\workgroups\np-behavior\VBN_video_analysis'
+baseDir = r'\\allen\programs\mindscope\workgroups\np-behavior\vbn_data_release\supplemental_tables'
+
+outputDir = r'\\allen\programs\mindscope\workgroups\np-behavior\VBN_video_analysis'
+
+stimTable = pd.read_csv(os.path.join(baseDir,'master_stim_table.csv'))
+
+sessionIds = stimTable['session_id'].unique()
 
 
+
+#
+nFlashes = []
+nLicks = []
+for sessionId in sessionIds:
+    stim = stimTable[(stimTable['session_id']==sessionId) & stimTable['active']].reset_index()
+    flashTimes,changeFlashes,nonChangeFlashes,lick = getFlashTimes(stim)
+    nFlashes.append(nonChangeFlashes.sum())
+    nLicks.append(lick[nonChangeFlashes].sum())
+nFlashes = np.array(nFlashes)
+nLicks = np.array(nLicks)
+lickProb = nLicks / nFlashes
+
+for d in (nFlashes,nLicks,lickProb):
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    dsort = np.sort(d)
+    cumProb = np.array([np.sum(d<=i)/d.size for i in dsort])
+    ax.plot(dsort,cumProb,'k')
+    for side in ('right','top'):
+        ax.spines[side].set_visible(False)
+    ax.tick_params(direction='out',top=False,right=False)
+    plt.tight_layout()
+    
+
+
+#
+
+for f in glob.glob(os.path.join(outputDir,'facemapData','*.hdf5')):
+    sessionId = os.path.basename(f)[:10]
+    fig = plt.figure(figsize=(3,6))
+    with h5py.File(f) as d:
+        x = d['xrange_bin'][()]
+        y = d['yrange_bin'][()]
+        
+        ax = fig.add_subplot(3,1,1)
+        ax.imshow(d['avgframe'][()][y,:][:,x],cmap='gray')
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_title(sessionId + '\n' + 'average frame')
+        
+        ax = fig.add_subplot(3,1,2)
+        ax.imshow(d['avgmotion'][()][y,:][:,x],cmap='gray')
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_title('average motion')
+        
+        ax = fig.add_subplot(3,1,3)
+        ax.imshow(np.average(d['motMask'][()],weights=d['motSv'][()],axis=2),cmap='bwr')
+        ax.set_xticks([])
+        ax.set_yticks([])
+        ax.set_title('weighted average\nmask')
+    plt.tight_layout()
+    plt.savefig(os.path.join(outputDir,'facemapRois',sessionId+'_roi.png'))
+    plt.close()
+    
+    
+#
+s = []
+for f in glob.glob(os.path.join(outputDir,'facemapLickDecoding','facemapLickDecoding_*.npy')):
+    d = np.load(f,allow_pickle=True).item()
+    a = d['balancedAccuracy']
+    if a[0] > 0.6 or a[-1] < 0.8:
+        s.append((os.path.basename(f)[-14:-5],a[0],a[-1]))
+
+
+#
 decodeWindowEnd = 0.75
 frameInterval = 1/60
 decodeWindows = np.arange(0,decodeWindowEnd+frameInterval/2,frameInterval)
 
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
-for f in glob.glob(os.path.join(baseDir,'facemapLickDecoding','facemapLickDecoding_*.npy')):
+for f in glob.glob(os.path.join(outputDir,'facemapLickDecoding','facemapLickDecoding_*.npy')):
     d = np.load(f,allow_pickle=True).item()
     ax.plot(decodeWindows,d['balancedAccuracy'],'k',alpha=0.25)
 for side in ('right','top'):
@@ -61,7 +136,7 @@ decodeWindows = np.arange(decodeWindowSize,decodeWindowEnd+decodeWindowSize,deco
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 d = {region: [] for region in regions}
-for f in glob.glob(os.path.join(baseDir,'unitLickDecoding','unitLickDecoding_*.npy')):
+for f in glob.glob(os.path.join(outputDir,'unitLickDecoding','unitLickDecoding_*.npy')):
     data = np.load(f,allow_pickle=True).item()
     for region in regions:
         if len(data[region]['balancedAccuracy'])>0:
