30b2f1fa3027faf9442e0e223d8fdb5fd9d85317
Sam Gale <samueldgale@gmail.com>
2023-10-11 18:11:22 -0700

    sdg

--- vbnFigs.py
+++ vbnFigs.py
@@ -443,8 +443,9 @@ plt.tight_layout()
 
 
 # response time prediction from integrator
-filePaths = glob.glob(os.path.join(outputDir,'integratorResponseTimes','integratorResponseTimes_*.hdf5'))
+filePaths = glob.glob(os.path.join(outputDir,'integratorResponseTimes','old','integratorResponseTimes_*.hdf5'))
 regions = ('VISp','VISl','VISrl','VISal','VISpm','VISam')
+flashLabels = ('change','preChange','catch','nonChange','omitted','prevOmitted')
 imageTypeLabels = ('familiar','familiarNovel','novel')
 imageTypeColors = 'kgm'
 t = np.arange(150)
@@ -452,30 +453,22 @@ t = np.arange(150)
 novelSession = []
 respMice = {lbl: [] for lbl in imageTypeLabels}
 respTimeMice = copy.deepcopy(respMice)
+respMiceRegions = {region: {flashes: {lbl: [] for lbl in imageTypeLabels} for flashes in flashLabels} for region in regions}
+respTimeMiceRegions = copy.deepcopy(respMiceRegions)
+spikeRate = copy.deepcopy(respMiceRegions)
+integrator = copy.deepcopy(respMiceRegions)
+respModel = copy.deepcopy(respMiceRegions)
+respTimeModel = copy.deepcopy(respMiceRegions)
 leak = {region: [] for region in regions}
 threshold = copy.deepcopy(leak)
 baseSpikeRate = copy.deepcopy(leak)
 maxSpikeRate = copy.deepcopy(leak)
-spikeRate = {region: {lbl: [] for lbl in imageTypeLabels} for region in regions}
-integrator = copy.deepcopy(spikeRate)
-respModel = copy.deepcopy(spikeRate)
-respTimeModel = copy.deepcopy(spikeRate)
 
 for i,f in enumerate(filePaths):
     print(i)
     with h5py.File(f,'r') as d:
-        change = d['change'][()]
         novel = d['novel'][()]
         novelSession.append(novel.any())
-        if novelSession[-1]:
-            inds = ((~novel & change),(novel & change))
-            lbls = imageTypeLabels[1:]
-        else:
-            inds = (change,)
-            lbls = (imageTypeLabels[0],)
-        for ind,lbl in zip(inds,lbls):
-            respMice[lbl].append(d['hit'][ind])
-            respTimeMice[lbl].append(d['lickLatency'][ind])
         for region in regions:
             if len(d[region]) > 0:
                 leak[region].append(d[region]['leak'][()])
@@ -484,49 +477,55 @@ for i,f in enumerate(filePaths):
                 maxSpikeRate[region].append(d[region]['maxSpikeRate'][()])
                 spRate = d[region]['spikeRate'][()]
                 intg = d[region]['integrator'][()]
-                r = d[region]['modelResp'][()]
-                rt = d[region]['modelRespTime'][()]
+            for flashes in flashLabels:
+                if flashes == 'preChange':
+                    flashInd = np.concatenate((d['change'][1:],[False]))
+                else:
+                    flashInd = d[flashes][()]
+                if novelSession[-1]:
+                    inds = ((flashInd & ~novel),(flashInd & novel))
+                    lbls = imageTypeLabels[1:]
+                else:
+                    inds = (flashInd,)
+                    lbls = (imageTypeLabels[0],)
                 for ind,lbl in zip(inds,lbls):
-                    spikeRate[region][lbl].append(spRate[ind].mean(axis=0))
-                    integrator[region][lbl].append(intg[ind].mean(axis=0))
-                    respModel[region][lbl].append(r[ind])
-                    respTimeModel[region][lbl].append(rt[ind])
-            else:
-                leak[region].append(np.nan)
-                threshold[region].append(np.nan)
-                baseSpikeRate[region].append(np.nan)
-                maxSpikeRate[region].append(np.nan)
-                for ind,lbl in zip(inds,lbls):
-                    spikeRate[region][lbl].append(np.full(t.size-1,np.nan))
-                    integrator[region][lbl].append(np.full(t.size,np.nan))
-                    respModel[region][lbl].append(np.array([]))
-                    respTimeModel[region][lbl].append(np.array([]))
+                    respMice[lbl].append(d['lick'][ind])
+                    respTimeMice[lbl].append(d['lickLatency'][ind])
+                    if len(d[region]) > 0:
+                        respMiceRegions[region][flashes][lbl].append(d['lick'][ind])
+                        respTimeMiceRegions[region][flashes][lbl].append(d['lickLatency'][ind])
+                        spikeRate[region][flashes][lbl].append(spRate[ind].mean(axis=0))
+                        integrator[region][flashes][lbl].append(intg[ind].mean(axis=0))
+                        respModel[region][flashes][lbl].append(d[region]['modelResp'][ind])
+                        respTimeModel[region][flashes][lbl].append(d[region]['modelRespTime'][ind])
 
 
 # plot mean spike rate
-fig = plt.figure(figsize=(8,8))
-for i,region in enumerate(regions):
-    ax = fig.add_subplot(len(regions),1,i+1)
-    for lbl,clr in zip(imageTypeLabels,imageTypeColors):
-        d = np.array(spikeRate[region][lbl])
-        m = np.nanmean(d,axis=0)
-        n = np.sum(~np.isnan(d[:,0]))
-        s = np.nanstd(d,axis=0)/(n**0.5)
-        ax.plot(t[1:],m,color=clr,label=lbl+' (n='+str(n)+')')
-        ax.fill_between(t[1:],m+s,m-s,color=clr,alpha=0.25)
-    for side in ('right','top'):
-        ax.spines[side].set_visible(False)
-    ax.tick_params(direction='out',top=False,right=False)
-    ax.set_xlim([0,t[-1]])
-    if i==len(regions)-1:
-        ax.set_xlabel('Time from change (ms)')
-    else:
-        ax.set_xticklabels([])
-    ax.set_ylabel('Spikes/s')
-    ax.set_title(region)
-    if i==0:
-        ax.legend(bbox_to_anchor=(1,1),loc='upper left')
-plt.tight_layout()
+for flashes in flashLabels:
+    fig = plt.figure(figsize=(8,8))
+    fig.suptitle(flashes)
+    for i,region in enumerate(regions):
+        ax = fig.add_subplot(len(regions),1,i+1)
+        for lbl,clr in zip(imageTypeLabels,imageTypeColors):
+            d = np.array(spikeRate[region][flashes][lbl])
+            m = np.nanmean(d,axis=0)
+            n = np.sum(~np.isnan(d[:,0]))
+            s = np.nanstd(d,axis=0)/(n**0.5)
+            ax.plot(t[1:],m,color=clr,label=lbl+' (n='+str(n)+')')
+            ax.fill_between(t[1:],m+s,m-s,color=clr,alpha=0.25)
+        for side in ('right','top'):
+            ax.spines[side].set_visible(False)
+        ax.tick_params(direction='out',top=False,right=False)
+        ax.set_xlim([0,t[-1]])
+        if i==len(regions)-1:
+            ax.set_xlabel('Time from change (ms)')
+        else:
+            ax.set_xticklabels([])
+        ax.set_ylabel('Spikes/s')
+        ax.set_title(region)
+        if i==0:
+            ax.legend(bbox_to_anchor=(1,1),loc='upper left')
+    plt.tight_layout()
 
 # plot mouse response rate and latency
 fig = plt.figure()
@@ -665,7 +664,7 @@ for i,region in enumerate(regions):
     ax = fig.add_subplot(len(regions),1,i+1)
     for ind,clr,lbl in zip((~novel,novel),'gm',('familiar-only sessions','novel sessions')):
         thresh = np.array(threshold[region])[ind] 
-        thresh *= np.array(maxSpikeRate[region])[ind]
+        #thresh *= np.array(maxSpikeRate[region])[ind]/1000
         dsort = np.sort(thresh[~np.isnan(thresh)])
         cumProb = np.array([np.sum(dsort<=i)/dsort.size for i in dsort])
         ax.plot(dsort,cumProb,color=clr,label=lbl)
@@ -674,7 +673,7 @@ for i,region in enumerate(regions):
     ax.tick_params(direction='out',top=False,right=False)
     ax.set_ylim([0,1.01])
     if i==len(regions)-1:
-        ax.set_xlabel('Integrator threshold (input spikes/s)')
+        ax.set_xlabel('Integrator threshold (spikes/neuron)')
     else:
         ax.set_xticklabels([])
     if i==len(regions)//2-1:
